d08eba33b95977b17cc8613818c68790bfb30027
Darian Tocci <dtocci@tulane.edu>
2026-09-04 14:24:49 -0500

    DAYB-32: Initialize project structure

---
+++ backend/src/main/java/com/daybuilder/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.daybuilder.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.daybuilder")
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daybuilder/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daybuilder.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
