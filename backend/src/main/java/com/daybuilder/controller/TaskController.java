c1017f252d799bbf788ce4732fe98c7bcd25f968
Darian Tocci <dtocci@tulane.edu>
2026-09-21 14:22:48 -0500

    DAYB-45 add POST tasks endpoint

--- backend/src/main/java/com/daybuilder/controller/TaskController.java
+++ backend/src/main/java/com/daybuilder/controller/TaskController.java
@@ -2,7 +2,11 @@ package com.daybuilder.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,11 @@ public class TaskController {
     @GetMapping
     public List<Task> getAllTasks() {
         return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 }
